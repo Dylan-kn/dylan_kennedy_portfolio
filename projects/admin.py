0f2839952d58f11965eac1af5b04dcf724262374
Dylan Kennedy <kennedy.dyp@gmail.com>
2025-04-03 11:21:49 -0700

    Initial setup of portfolio structure with projects app

---
+++ projects/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Project
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = {'title', 'is_featured'}
+
+
+
---
+++ projects/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.utils.text import slugify
+
+class Project(models.Model): 
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    tech_stack = models.CharField(max_length=200)
+    github_link = models.URLField()
+    live_demo_link = models.URLField(blank=True, null=True)
+    image = models.ImageField(upload_to='project_images/', blank=True, null=True)
+    is_featured = models.BooleanField(default=False)
+    slug = models.SlugField(unique=True, blank=True)
+
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
