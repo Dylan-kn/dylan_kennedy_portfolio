352b9a962173a931417c52f06978336554cdabe5
Dylan Kennedy <kennedy.dyp@gmail.com>
2025-04-06 11:34:36 -0700

    Adjusted allowed hosts in settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://dylan-kennedy-portfolio.onrender.com/']
 
 
 # Application definition
