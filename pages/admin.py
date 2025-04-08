4523d5608608be52fecb0850a0a39c7caec5d730
Dylan Kennedy <kennedy.dyp@gmail.com>
2025-04-08 11:07:00 -0700

    fixed pages admin

--- pages/admin.py
+++ pages/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from .models import ContactMessage, Skills
+from .models import ContactMessage, Skill
 
 @admin.register(ContactMessage)
 class ContactMessageAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'submitted_at')
     search_fields = ('name', 'email', 'message')
 
-@admin.register(Skills)
+@admin.register(Skill)
 class SkillsAdmin(admin.ModelAdmin):
     list_display = ('name', 'show_on_homepage')
